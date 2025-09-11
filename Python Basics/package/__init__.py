846efbff11d8966d0279a3f4d6d816406054fdd7
YashChoudhary13 <yashchoudhary13@outlook.com>
2025-09-12 03:15:27 +0530

    Day 4

---
+++ Python Basics/package/__init__.py
@@ -0,0 +1,5 @@
+# This needs to be first file in our package folder
+'''__init__.py is a special file used in Python to define packages and initialize their namespaces
+__init__.py is a special file that tells Python “this directory should be treated as a package”.
+Without it (before Python 3.3), Python wouldn’t recognize the folder as a package you could import.
+Any code inside __init__.py runs automatically when the package is imported.'''
